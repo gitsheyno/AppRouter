95847686d36bfaf1a900a68da149d3ca586513ff
shayan heidary <hdrydeveloper@gmail.com>
2023-10-21 18:27:18 +0200

    dynamic metadata added

--- app/users/[userid]/page.tsx
+++ app/users/[userid]/page.tsx
@@ -2,25 +2,39 @@ import getUser from "@/lib/getUser";
 import getUserPosts from "@/lib/getUserPosts";
 import UserPosts from "./components/UserPosts";
 import { Suspense } from "react";
+import { Metadata } from "next";
+
 type Params = {
   params: {
     userid: string;
   };
 };
 
+export async function generateMetadata({
+  params: { userid },
+}: Params): Promise<Metadata> {
+  const userData: Promise<User> = getUser(userid);
+  const user: User = await userData;
+
+  return {
+    title: user.name,
+    description: `this is a page of${user.name}`,
+  };
+}
+
 export default async function UserPage({ params: { userid } }: Params) {
   const userData: Promise<User> = getUser(userid);
   const userPostsData: Promise<Post[]> = getUserPosts(userid);
 
   //   const [user, userPosts] = await Promise.all([userData, userPostsData]);
-  const user = await userData;
+  const user: User = await userData;
 
   return (
     <>
       <h2>{user.name}</h2>
       <br />
       <Suspense fallback={<h2>Loading...</h2>}>
-        {/* @ts-expect-error Server Component */}
+        {/* @ts-expect-error server component */}
         <UserPosts promise={userPostsData} />
       </Suspense>
     </>
